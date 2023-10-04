3c564c70250e1156643a941d3f2921eb12520a08
Isaiah-Mwinga <motongori@marketforce360.com>
2023-10-04 08:52:03 +0300

    faceID

--- ImageID/Image/models.py
+++ ImageID/Image/models.py
@@ -4,3 +4,4 @@ class User(models.Model):
     username = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
     face_encoding = models.BinaryField(null=True, blank=True)
+    image = models.ImageField(upload_to='captured_images/')
